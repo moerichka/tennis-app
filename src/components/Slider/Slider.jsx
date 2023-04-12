77308517838d5022408ca56a362758f95fc8c5c7
MichailI <mishika1221@mail.ru>
2023-04-12 21:08:27 +0600

    Реализована компонента swiper

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
 import s from "./Slider.module.scss";
 
-function Slider() {
-  return <div>Slider</div>;
+function Slider({ slides = [] }) {
+  const [swiper, setSwiper] = useState(null);
+
+  const onPrev = () => {
+    swiper.slidePrev();
+  };
+
+  const onNext = () => {
+    swiper.slideNext();
+  };
+
+  return (
+    <div className={s.slider}>
+      <div className={s.topPart}>
+        <div className={s.title}>Фото кортов</div>
+        <div className={s.buttons}>
+          <button className={s.button} onClick={onPrev}>
+            prev
+          </button>
+          <button className={s.button} onClick={onNext}>
+            next
+          </button>
+        </div>
+      </div>
+      <div className={s.swiperWrapper}>
+        <Swiper
+          loop
+          grabCursor
+          className={s.swiper}
+          onSwiper={(swiper) => {
+            setSwiper(swiper);
+          }}
+        >
+          {slides.map((elem) => (
+            <SwiperSlide className={s.slide} key={elem.id}>
+              {elem.image && (
+                <img className={s.image} src={elem.image} alt="" />
+              )}
+              {elem.description && (
+                <div className={s.subtitle}>{elem.description}</div>
+              )}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 }
 
 export default Slider;
--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,15 +2,34 @@ import React from 'react'
 
 import Slider from '../../components/Slider'
 
+import cort1 from "../../images/cort1.jpg"
+import cort2 from "../../images/cort2.jpg"
+
 import s from "./AboutPage.module.scss"
 
 function AboutPage() {
+
+  const slides = [
+    {
+      id: 0,
+      image: cort1,
+      description: "Голубой корт"
+    },
+    {
+      id: 1,
+      image: cort2,
+      description: "Закрытый корт"
+    },
+  ]
+
   return (
     <div>
       <div className={s.header}>Header</div>
 
       <div className={s.main}>
-        <Slider />
+        <div className={s.container}>
+          <Slider slides={slides} />
+        </div>
       </div>
 
       <div className={s.footer}>Footer</div>
